e6835fbf2876ce8853a477d67e6c271c9d5e382e
AmirKhoueiry <89817298+AmirKhoueiry24@users.noreply.github.com>
2021-09-11 00:41:21 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+   
+   var name = Console.ReadLine();//Ask for name 
+     Console.WriteLine ("Who's going to win?");
+   Console.WriteLine (name);
+  
+  
+  
+  }
+}
